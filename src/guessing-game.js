f7a7b98a4cb102638352d7d1e001517be85b0ddd
parechkina <t.parechkina@gmail.com>
2017-02-01 23:45:32 +0300

    Implement GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,39 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.max = null;
+    	this.min = null;
+    	this.quantity = 0;
+    }
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
     }
 
     guess() {
+    	return this.findMedium();
 
     }
 
     lower() {
+    	this.max=this.findMedium();
 
     }
 
-    greater() {
+    greater() {    	
+    	this.min=this.findMedium();
+    }
+
+    findMedium() {
+    	var quantity = this.max-this.min+1;
+    	var medium = 0;
+    	if(quantity%2==0){
+    		medium = quantity/2+this.min;
+    	}
+    	else {
+    		medium = parseInt(quantity/2)+this.min;
+    	}
+    	return medium;
 
     }
 }
